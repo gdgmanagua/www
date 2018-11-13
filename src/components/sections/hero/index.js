602367b15a78663fd8ef800d6fa2177bfccd4e68
Oscar Cortez <om.cortez.2010@gmail.com>
2018-11-12 22:22:52 -0600

    Improve hero style

--- src/components/sections/hero/index.js
+++ src/components/sections/hero/index.js
@@ -1,5 +1,7 @@
-import { Component } from 'preact';
-import style from './style.css';
+import { Component } from 'preact'
+import style from './style.css'
+
+import { Button } from 'preact-fluid';
 
 export default class Intro extends Component {
   render() {
@@ -7,8 +9,9 @@ export default class Intro extends Component {
       <section className={style.hero}>
         <div className={style.hero_wrap}>
             <div className={style.hero_content}>
-              <h2>Se un heroe. Se un GDG!</h2>
-              <h1>Únete al GDG Managua</h1>
+              <h2 className={style.hero_subtitle}>Se un heroe. Se un GDG!</h2>
+              <h1 className={style.hero_title}>Únete al GDG Managua.</h1>
+              <Button size='normal' rounded>Empecemos!</Button>
             </div>
         </div>
       </section>
