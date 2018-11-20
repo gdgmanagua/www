e93dff5487480c5177845bf49bf5e8769de133e3
Oscar Cortez <om.cortez.2010@gmail.com>
2018-11-19 21:08:38 -0600

    Finish hero section #6 Remove preact-fluid in favor of just Bulma css

--- src/components/sections/hero/index.js
+++ src/components/sections/hero/index.js
@@ -1,7 +1,7 @@
 import { Component } from 'preact'
 import style from './style.css'
 
-import { Button } from 'preact-fluid';
+// import { Button } from 'preact-fluid';
 
 export default class Intro extends Component {
   render() {
@@ -11,7 +11,7 @@ export default class Intro extends Component {
             <div className={style.hero_content}>
               <h2 className={style.hero_subtitle}>Se un heroe. Se un GDG!</h2>
               <h1 className={style.hero_title}>Únete al GDG Managua.</h1>
-              <Button size='normal' rounded>Empecemos!</Button>
+              <button className="button is-primary is-inverted is-outlined is-large">Empecemos!</button>
             </div>
             <svg className={style.cursor} xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 25.166666 37.8704414" enable-background="new 0 0 25.166666 37.8704414" xmlSpace="preserve">
               <path fill="none" stroke="#FFF" stroke-width="2.5" stroke-miterlimit="10" d="M12.5833445 36.6204414h-0.0000229C6.3499947 36.6204414 1.25 31.5204487 1.25 25.2871208V12.5833216C1.25 6.3499947 6.3499951 1.25 12.5833216 1.25h0.0000229c6.2333269 0 11.3333216 5.0999947 11.3333216 11.3333216v12.7037992C23.916666 31.5204487 18.8166714 36.6204414 12.5833445 36.6204414z"></path>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import '../node_modules/bulma/css/bulma.min.css'
 import './style';
 import App from './components/app';
 
