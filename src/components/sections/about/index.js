644d299c3cf9443c601a5e0fac2c3caae9b9b3c6
Oscar Cortez <om.cortez.2010@gmail.com>
2018-11-19 22:04:23 -0600

    Initial work on about section #8

--- src/components/sections/about/index.js
+++ src/components/sections/about/index.js
@@ -1,33 +1,28 @@
 import { Component } from 'preact';
-import { Card, Layout, Grid, Cell, Image } from 'preact-fluid';
 import style from './style.css';
+
 
 export default class About extends Component {
   render() {
     return(
-      <section className={style.about}>
-        <Grid columns={2}>
-          <Cell width={1}>
-            <Image responsive={[
-                    {
-                      src: "https://colorlib.com/preview/theme/cryptocurrency/img/about-img.png",
-                      media: "(min-width: 320px)"
-                    }
-                ]}
-                placeholder=""
-                src=""
-            />
-          </Cell>
-        <Cell>
-          The GDG platform is a membership level within the wider Google Developers Community Groups
-          program and it offers tailored support to organizers of local GDG chapters.
-
-          GDGs are local groups of developers who are specifically interested in Google products and APIs.
-          Each local group is called a GDG chapter and can host a variety of technical activities for
-          developers - from just a few people getting together to watch our latest videos, to large gatherings with demos
-          and tech talks, to hackathons.
-        </Cell>
-        </Grid>
+      <section className={`section ${style.about}`}>
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <figure className="image">
+                <img src="https://secure.meetupstatic.com/photos/event/8/8/e/e/600_471995054.jpeg" />
+              </figure>
+            </div>
+            <div className="column content">
+              <h2 className="is-size-1 has-text-black has-text-centered">¿Que es un GDG?</h2>
+              <p className="has-text-justified">Los <b>GDG</b> son grupos locales de desarrolladores que están específicamente interesados en los productos y las API de <b>Google</b>.</p>
+              <p className="has-text-justified">Cada grupo local se denomina capítulo GDG y puede albergar una variedad de actividades técnicas para desarrolladores,
+               desde solo unas pocas personas que se reúnen para ver nuestros últimos videos, grandes reuniones con demostraciones y charlas tecnológicas, y hackathons.</p>
+              <p className="has-text-justified">La plataforma <b>GDG</b> es un nivel de membresía dentro del programa más amplio de Grupos de la <i>Comunidad de Desarrolladores de Google</i>
+               y ofrece soporte personalizado para los organizadores de los capítulos locales de GDG.</p>
+            </div>
+          </div>
+        </div>
       </section>
     )
   }
