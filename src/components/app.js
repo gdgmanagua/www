c8d9a83d3b43269864a5968c5c2c8422770077f5
Oscar Cortez <om.cortez.2010@gmail.com>
2018-11-21 22:36:14 -0600

    Initial work on sponsor section component #18

--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,20 @@
-import { Component } from 'preact';
+import { Component } from 'preact'
 
-import Intro from './sections/intro';
-import About from './sections/about';
+import Intro from './sections/intro'
+import About from './sections/about'
+import Newsletter from './sections/newsletter'
+import Sponsor from './sections/sponsor'
+
 
 export default class App extends Component {
-  /** Gets fired when the route changes.
-   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-   *	@param {string} event.url	The newly routed URL
-   */
-  handleRoute = e => {
-    this.currentUrl = e.url;
-  };
-
   render() {
     return (
       <main id="app">
         <Intro />
         <About />
+        <Newsletter />
+        <Sponsor />
       </main>
-    );
+    )
   }
 }
---
+++ src/components/sections/sponsor/index.js
@@ -0,0 +1,97 @@
+import { Component } from 'preact';
+import style from './style.css';
+
+
+export default class Sponsor extends Component {
+  render() {
+    return(
+      <section className="section">
+        <div className="container">
+          <h1 class="title has-text-centered">Regional</h1>
+          <div className="columns">
+            <div className="column">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">Google Developers</p>
+                </header>
+                <div class="card-content">
+                  <figure class="image">
+                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
+                  </figure>
+                </div>
+                <footer class="card-footer">
+                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
+                </footer>
+              </div>
+            </div>
+            <div className="column">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">Google Developers</p>
+                </header>
+                <div class="card-content">
+                  <figure class="image">
+                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
+                  </figure>
+                </div>
+                <footer class="card-footer">
+                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
+                </footer>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <h1 class="title has-text-centered">Local</h1>
+          <div className="columns">
+            <div className="column">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">Google Developers</p>
+                </header>
+                <div class="card-content">
+                  <figure class="image">
+                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
+                  </figure>
+                </div>
+                <footer class="card-footer">
+                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
+                </footer>
+              </div>
+            </div>
+            <div className="column">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">Google Developers</p>
+                </header>
+                <div class="card-content">
+                  <figure class="image">
+                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
+                  </figure>
+                </div>
+                <footer class="card-footer">
+                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
+                </footer>
+              </div>
+            </div>
+            <div className="column">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">Google Developers</p>
+                </header>
+                <div class="card-content">
+                  <figure class="image">
+                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
+                  </figure>
+                </div>
+                <footer class="card-footer">
+                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
+                </footer>
+              </div>
+            </div>
+          </div>
+          <a href="#contact" className="button is-black is-outlined is-rounded">Colaborar</a>
+        </div>
+      </section>
+    )
+  }
+}
