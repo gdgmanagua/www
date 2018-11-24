9d502271783fae234ba8d6830f6f1525aaa47414
Jonathan Guadamuz <guadamuzjj@gmail.com>
2018-11-23 22:47:48 -0600

    Add sponsor logos and improve style

--- src/components/sections/sponsor/index.js
+++ src/components/sections/sponsor/index.js
@@ -1,6 +1,26 @@
 import { Component } from 'preact';
 import style from './style.css';
 
+import LogoGDG from '../../../assets/sponsors/gdg1.png';
+import LogoWTM from '../../../assets/sponsors/wtm1.png';
+import LogoRain from '../../../assets/sponsors/rain1.png';
+import LogoWaffle from '../../../assets/sponsors/waffle-studio1.png';
+
+const SponsorCard = (props) => {
+  return (
+    <div className={style.sponsor_card}>
+      <header className={style.sponsor_header}>
+        <p className={style.sponsor_header_title}>{props.name}</p>
+      </header>
+      <div className={style.sponsor_content}>
+        <img className={style.sponsor_image} src={props.logo} alt={`Logo ${props.name}`} />
+      </div>
+      <footer className={style.sponsor_footer}>
+        <a target="_blank" href={props.website} class="card-footer-item">Sitio</a>
+      </footer>
+    </div>
+  )
+}
 
 export default class Sponsor extends Component {
   render() {
@@ -10,83 +30,32 @@ export default class Sponsor extends Component {
           <h1 class="title has-text-centered">Regional</h1>
           <div className="columns">
             <div className="column">
-              <div class="card">
-                <header class="card-header">
-                  <p class="card-header-title">Google Developers</p>
-                </header>
-                <div class="card-content">
-                  <figure class="image">
-                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
-                  </figure>
-                </div>
-                <footer class="card-footer">
-                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
-                </footer>
-              </div>
+              <SponsorCard 
+                name="Google Developers"
+                logo={LogoGDG}
+                website="https://developers.google.com/" />
             </div>
             <div className="column">
-              <div class="card">
-                <header class="card-header">
-                  <p class="card-header-title">Google Developers</p>
-                </header>
-                <div class="card-content">
-                  <figure class="image">
-                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
-                  </figure>
-                </div>
-                <footer class="card-footer">
-                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
-                </footer>
-              </div>
+              <SponsorCard 
+                name="Women Tech Makers"
+                logo={LogoWTM}
+                website="https://www.womentechmakers.com/" />
             </div>
           </div>
           <hr />
           <h1 class="title has-text-centered">Local</h1>
           <div className="columns">
             <div className="column">
-              <div class="card">
-                <header class="card-header">
-                  <p class="card-header-title">Google Developers</p>
-                </header>
-                <div class="card-content">
-                  <figure class="image">
-                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
-                  </figure>
-                </div>
-                <footer class="card-footer">
-                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
-                </footer>
-              </div>
+              <SponsorCard 
+                name="Rain Agency"
+                logo={LogoRain}
+                website="https://rain.agency/" />
             </div>
             <div className="column">
-              <div class="card">
-                <header class="card-header">
-                  <p class="card-header-title">Google Developers</p>
-                </header>
-                <div class="card-content">
-                  <figure class="image">
-                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
-                  </figure>
-                </div>
-                <footer class="card-footer">
-                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
-                </footer>
-              </div>
-            </div>
-            <div className="column">
-              <div class="card">
-                <header class="card-header">
-                  <p class="card-header-title">Google Developers</p>
-                </header>
-                <div class="card-content">
-                  <figure class="image">
-                    <img src="https://www.iotdevfest.com/files/2015/11/google_developers_icon_vertical-e1449209914240-1024x614.png" alt="Google Developers" />
-                  </figure>
-                </div>
-                <footer class="card-footer">
-                  <a href="https://developers.google.com/" class="card-footer-item">Sitio</a>
-                </footer>
-              </div>
+              <SponsorCard 
+                name="Waffle Studio"
+                logo={LogoWaffle}
+                website="https://www.waffle.studio/" />
             </div>
           </div>
           <a href="#contact" className="button is-black is-outlined is-rounded">Colaborar</a>
