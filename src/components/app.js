f6598d638dc0dfdae9bd3229728ddc7abb11e52d
Oscar Cortez <om.cortez.2010@gmail.com>
2018-11-23 22:36:10 -0600

    Initial work on footer section component #25

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import { Component } from 'preact';
 
 import Intro from './sections/intro';
 import About from './sections/about';
+import Footer from './sections/footer'
 
 export default class App extends Component {
   /** Gets fired when the route changes.
@@ -17,6 +18,7 @@ export default class App extends Component {
       <main id="app">
         <Intro />
         <About />
+        <Footer />
       </main>
     );
   }
